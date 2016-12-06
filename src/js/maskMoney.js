553e61167bc2969993d327a819d762911393f9d3
Renanmacedo <renan.aquinolima@gmail.com>
2016-12-06 08:28:46 -0300

    criando o codigo js

--- src/js/maskMoney.js
+++ src/js/maskMoney.js
@@ -1,8 +1,17 @@
+/*
+ Built 1.0.0
+*/
 (function(d){
  var _value;
- var _arr  = [];
+ var _test;
+ var _regExp	=	/^\d{3}/g;
  var mask = function(inputValue){
+ 	_value = document.getElementById(inputValue).value;
+ 	_test = _regExp.test(_value);
+ 	if(_test == false) return _test;
+ 	var result = _regExp.exec(_value);
 
+ 	var _f = document.getElementById(inputValue).innerHTML = result;
  }
   d.mask = mask;	
-})(document)
+})(document);
